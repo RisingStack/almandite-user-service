c55f5fdd9dc61b67f30f8a1f19c68139207f77c2
Peter Czibik <p.czibik@gmail.com>
2018-03-01 14:07:45 +0100

    docs(healthcheck): add documentation for healthcheck handler

--- handlers/Healthcheck.go
+++ handlers/Healthcheck.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// Healthcheck handles "/healthcheck"
 func Healthcheck(rw http.ResponseWriter, r *http.Request) {
 	rw.Header().Set("content-type", "application/json")
 	json.NewEncoder(rw).Encode(
